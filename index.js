607dd29ba99b8efa3768190035d5f1d576e16064
Azizul Hakim <azizulcse16@gmail.com>
2025-09-09 18:27:12 +0600

    my 4th commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let price = 0;
+const loadCategory = () => {
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then(res => res.json())
+    .then(data => {
+        displaycatebar(data.categories); 
+    })
+}
+
+const displaycatebar = (categories) => {
+    const sidebar = document.getElementById("left");
+
+    // HTML assemble 
+    let html = `<h2 class="text-2xl font-bold text-black">Categories</h2>
+    <ul id="category-list">
+        <li id="all" class="list-none hover:text-white hover:bg-green-500 rounded-md px-3 py-2 cursor-pointer active-category">All Trees</li>`;
+
+    categories.forEach(cat => {
+        html += `<li id="cat-${cat.id}" class="category-item list-none px-3 py-2 m-3 hover:bg-green-500 hover:text-white rounded-md cursor-pointer">
+                    ${cat.category_name}
+                 </li>`;
+    });
+    html += `</ul>`;
+    sidebar.innerHTML = html;
+    
